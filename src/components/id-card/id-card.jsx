c239d68b19d6cbba200abc49b92ce5eaa34da886
Andreu Monforte <andreump5@gmail.com>
2026-05-09 13:12:19 +0200

    feat: Iteration 1 done, need fix date order

---
+++ src/components/id-card/id-card.jsx
@@ -0,0 +1,33 @@
+import "./id-card.css";
+
+function IdCard(props) {
+  return (
+    <div className="id-card">
+      <img src={props.picture} />
+      <div>
+        <div>
+          <strong>First Name:</strong>
+          <span> {props.firstName}</span>
+        </div>
+        <div>
+          <strong>Last Name:</strong>
+          <span> {props.lastName}</span>
+        </div>
+        <div>
+          <strong>Gender:</strong>
+          <span> {props.gender}</span>
+        </div>
+        <div>
+          <strong>Height:</strong>
+          <span> {(props.height / 100).toFixed(2)}m</span>
+        </div>
+      </div>
+      <div>
+        <strong>Birth:</strong>
+        <span> {props.birth.toDateString()}</span>
+      </div>
+    </div>
+  );
+}
+
+export default IdCard;
